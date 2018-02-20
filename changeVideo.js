0ed41946278613e4919f16a1bf09232446d16116
KillQu3 <36411727+KillQu3@users.noreply.github.com>
2018-02-20 01:44:31 +0100

    Add files via upload

---
+++ changeVideo.js
@@ -0,0 +1,11 @@
+$( ".video_1" ).click(function() {
+  $(".videoPrincipal").attr("src","video1.mp4")
+});
+
+$( ".video_2" ).click(function() {
+  $(".videoPrincipal").attr("src","video2.mp4")
+});
+
+$( ".video_3" ).click(function() {
+  $(".videoPrincipal").attr("src","video3.mp4")
+});
